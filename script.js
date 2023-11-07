d32a2b63f2fbd465cb8d6e0133c36d3ae421415e
Md Nazmul Hossain <mdnazmulhossainbd85@gmail.com>
2023-11-07 11:30:14 +0600

    create default score and hide image

--- script.js
+++ script.js
@@ -20,68 +20,11 @@ const newGame = document.querySelector('.btn--new');
 const rollDice = document.querySelector('.btn--roll');
 const holdGame = document.querySelector('.btn--hold');
 
-// by default 
-let playing, currentScore, activePlayer,scores;
+// by default
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
 
-const reset = () => {
-    score0El.textContent=0;
-    score1El.innerText=0;
-    currentScore = 0;
-    activePlayer = 0;
-    scores = [0,0];
-    playing = true;
-
-    diceEl.classList.add('hidden');
-    player0El.classList.remove('player--winner');
-    player1El.classList.remove('player--winner');
-    player0El.classList.add('player--active');
-    player1El.classList.remove('player--active');
-}
-
-reset();
-
-const switchPlayer =  ()=>{
-    document.getElementById(`current--${activePlayer}`).textContent = 0; 
-        currentScore = 0;
-        activePlayer = activePlayer === 0 ? 1:0;
-        player0El.classList.toggle('player--active');
-        player1El.classList.toggle('player--active');
-}
-
-rollDice.addEventListener('click',function(){
-    if(playing){
-    const randomDice = Math.trunc(Math.random() * 6) + 1;
-    diceEl.classList.remove('hidden');
-    diceEl.src= `dice-${randomDice}.png`
-    if(randomDice !== 1){
-      currentScore += randomDice;
-      document.getElementById(`current--${activePlayer}`).textContent = currentScore; 
-    }
-    else{
-       switchPlayer();  
-    }
-    }
+rollDice.addEventListener('click', function () {
+  const randomDice = Math.floor(Math.random() * 6) + 1;
 });
-
-holdGame.addEventListener('click', function(){
-    if(playing){
-    scores[activePlayer] += currentScore;
-    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
-     
-    if(scores[activePlayer] >= 20){
-        playing = false;
-        diceEl.classList.add('hidden');
-        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
-        document
-        .querySelector(`.player--${activePlayer}`)
-        .classList.remove('player--active');
-    }  else{
-        switchPlayer();
-    }
-    } 
-});
-
-
-newGame.addEventListener('click', function(){
- reset();
-})
